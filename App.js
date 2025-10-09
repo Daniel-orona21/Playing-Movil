645473abc66bb9852d4e3f9c1e07d6c42b0f060f
Alejandor Daniel López Orona <128829840+Daniel-orona21@users.noreply.github.com>
2025-10-09 00:56:19 -0600

    avance login

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 import { Colors } from './constants/Colors';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
 
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
+
+  const [fontsLoaded, fontError] = useFonts({
+    'Michroma-Regular': require('./assets/fonts/Michroma-Regular.ttf'),
+    'Onest-Regular': require('./assets/fonts/Onest-Regular.ttf'),
+    'Onest-Bold': require('./assets/fonts/Onest-Bold.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={styles.titulo}>PLAYING</Text>
+      <Image source={require('./assets/imagenes/logo-5.png')} style={styles.logo} />
+      <View>
+        <Text>Inicia Sesión</Text>
+        <Button
+          title="Continua con Google"
+          onPress={() => {
+            console.log('Botón presionado');
+          }}
+        />
+      </View>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -14,8 +45,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: Colors.white,
+    display: 'flex',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo: {
+    fontFamily: 'Michroma-Regular',
+    fontWeight: '900',
+    position: 'absolute',
+    fontSize: 32,
+    color: 'white',
+    top: '10%',
+  },
+  logo: {
+    position: 'absolute',
+    marginTop: '100%',
+    transform: [
+      { scale: 1.05 }
+    ],
+    overflow: 'visible',
+    width: '100%',
+    objectFit: 'contain'
+  }
 });
